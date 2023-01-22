81de749e48e709627e2480e2eba76b64136d12a8
Vlad Gareev <gar.vlad.ix@gmail.com>
2023-01-22 23:43:36 +0400

    changed the method of access to API

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 export const isProd = process.env.NODE_ENV === 'production';
 
-export const API_URL = isProd ? process.env.REACT_PUBLIC_API_URL : 'http://localhost:80';
+export const API_URL = isProd ? 'https://lotos-test-back.onrender.com' : 'http://localhost:80';
