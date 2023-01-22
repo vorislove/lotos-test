1c208dc30a3d6ed08896304fabf628f6cd674c47
Vlad Gareev <gar.vlad.ix@gmail.com>
2023-01-22 19:39:02 +0400

    final app

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,19 +1,16 @@
 import { configureStore, ThunkAction, Action, combineReducers } from '@reduxjs/toolkit';
-import { userAPI } from '../services/users.service';
 import messageSlice from './slice/messageSlice';
 import timerSlice from './slice/timerSlice';
 import usersSlice from './slice/usersSlice';
 
 const rootReducer = combineReducers({
-	[userAPI.reducerPath]: userAPI.reducer,
 	timer: timerSlice,
 	users: usersSlice,
 	message: messageSlice
 });
 
 export const store = configureStore({
-	reducer: rootReducer,
-	middleware: (gDM) => gDM().concat(userAPI.middleware)
+	reducer: rootReducer
 });
 
 export type AppDispatch = typeof store.dispatch;
